d3b1c2eefa9862322306fe3cb096b930af1eb3da
Red-Thirten <rehlmgaming@gmail.com>
2021-03-18 18:57:58 -0700

    Remove Python (#1)
    * Update entrypoint.sh
    
    Remove python startup
    
    * Update Dockerfile
    
    Remove python package

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM openrct2/openrct2-cli:develop
 USER root
 
 RUN adduser --quiet --home /home/container container
-RUN apt-get install --no-install-recommends -y python3
 
 USER container
 ENV  USER=container HOME=/home/container
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-export PTY_STARTUP=${MODIFIED_STARTUP}
-python -c 'import pty; import os; pty.spawn(os.environ["PTY_STARTUP"].split(" "))'
+${MODIFIED_STARTUP}
